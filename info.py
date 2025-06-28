85d2f3ab5d2d75077ea938831d32979eda408b90
threatthreat <grezy8007@gmail.com>
2025-06-28 07:31:32 -0700

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ DATABASE_URI = environ.get("DATABASE_URI", "")
 DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
 
 # Files index database url
-FILES_DATABASE = environ.get("FILES_DATABASE", "")
+FILES_DATABASE = environ.get("FILES_DATABASE", "mongodb+srv://heisenberg:walder1234@cluster0.wkum1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 COLLECTION_NAME = environ.get("COLLECTION_NAME", "SilentXBotz_files")
 
 # Other Channel's
